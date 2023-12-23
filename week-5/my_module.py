3148e187063d5168bbdb290f1fcdf87433cb44c2
tubaakbas <tubakbas1@gmail.com>
2023-12-23 10:53:06 +0300

    added python concept

---
+++ week-5/my_module.py
@@ -0,0 +1,11 @@
+
+def my_function():
+    print("hello")
+    
+def call_api(request):
+    pass
+    
+if __name__=="__main__":
+    my_function()
+    request=""
+    call_api(request)
---
+++ week-5/run.py
@@ -0,0 +1,5 @@
+from my_module import my_function
+
+#name'in karşılığında main varsa bu fonks çalıştır
+#dediğimiz için hello yazmadı.
+#print(my_function.__name__)
